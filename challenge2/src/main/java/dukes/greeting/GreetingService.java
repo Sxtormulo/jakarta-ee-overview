efee16d3e6f655adcfece946c02a8bac4c158045
Sxtormulo <gabriel.a.soto0711@gmail.com>
2026-05-08 10:38:30 -0600

    Finish GreetingService methods

--- challenge2/src/main/java/dukes/greeting/GreetingService.java
+++ challenge2/src/main/java/dukes/greeting/GreetingService.java
@@ -1,23 +1,51 @@
 package dukes.greeting;
 
 // The GreetingService should be a Stateless session bean
-// Hint: Check out the @Stateless annotation
-public class GreetingService {
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.time.LocalDate;
+
+/** Provide a greeting service that manages greeting repository */
+@Stateless
+public class GreetingService
+{
+
+    private static final Logger logger = System.getLogger(GreetingService.class
+        .getName());
+
+    @Inject
+    private GreetingRepository greetings;
 
     /**
-     * Helpful documentation:
-     * https://jakarta.ee/specifications/platform/10/apidocs/jakarta/ejb/stateless
-     * https://jakarta.ee/specifications/platform/10/apidocs/jakarta/inject/package-summary.html
-     * https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/stream/package-summary.html
+     * Find a {@code Greeting} and map it to a {@code GreetingRecord}. If not record is
+     * found return a generated default
+     *
+     * @return the GreetingRecord with current Date
      */
+    public GreetingRecord findFirstGreetingRecord()
+    {
+        return greetings.findAll().stream()
+            .findFirst()
+            .map(g -> new GreetingRecord(g.getMessage(), LocalDate.now()))
+            .orElseGet(() ->
+            {
+                logger.log(Level.DEBUG, "Returning default greeting");
+                return new GreetingRecord("Hello, World!", LocalDate.now());
+            });
+    }
 
-    // inject the GreetingRepository
+    /**
+     * Persist a {@code GreetingRecord} with JPA
+     *
+     * @param greetingRecord a record to persist
+     * @return a new Greeting that was persisted
+     */
+    public Greeting persistGreeting(GreetingRecord greetingRecord)
+    {
+        final var greeting = new Greeting(greetingRecord.message());
+        return greetings.saveGreeting(greeting);
+    }
 
-    // Create a method that calls the GreetingRepository to find all greetings, chooses the first one,
-    // and maps it to a DTO in the form of a Record
-    // If no greetings are returned from the repository, return a hard coded one with the text "Hello, World!"
-    // and today's date.
-
-    // Add a method that takes a GreetingRecord as parameter, maps it to a Greeting,
-    // and calls the saveGreeting method on the GreetingRepository
 }
