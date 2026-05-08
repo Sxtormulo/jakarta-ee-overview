98ec7cd7bad2230a3f58b6ac07ae5976b5ac313c
Sxtormulo <gabriel.a.soto0711@gmail.com>
2026-05-08 10:40:02 -0600

    Make project modular

---
+++ challenge2/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+
+module dukes.greeting.challenge.two
+{
+
+    requires static jakarta.persistence;
+    requires static jakarta.ejb;
+    requires static jakarta.ws.rs;
+    exports dukes.greeting;
+}
