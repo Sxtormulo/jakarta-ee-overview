414defd384b5c9446c69391719b9526a9d8672d9
Sxtormulo <gabriel.a.soto0711@gmail.com>
2026-05-08 10:38:52 -0600

    Finish GreetingService Rest enpoints

--- challenge2/src/main/java/dukes/greeting/GreetingResource.java
+++ challenge2/src/main/java/dukes/greeting/GreetingResource.java
@@ -1,20 +1,64 @@
 package dukes.greeting;
 
 // request scoped
-// set the path to /greeting
-public class GreetingResource {
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.lang.System.Logger;
+import java.time.LocalDate;
+
+import static java.lang.System.Logger.Level.INFO;
+
+/** GreetingResources end points for {@code Greeting} entity */
+@RequestScoped
+@Path("/greeting")
+public class GreetingResource
+{
+
+    private static final Logger logger = System.getLogger(GreetingResource.class
+        .getName());
+
+    @EJB
+    private GreetingService greetingService;
 
     /**
-     * Helpful documentation:
-     * https://jakarta.ee/specifications/platform/10/apidocs/jakarta/ws/rs/package-summary.html
+     * Gets a Greeting record as a Json and save it to the db
+     *
+     * @param greetingRecord the record to persist
+     * @return the persisted Greeting
      */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public GreetingRecord createGreeting(GreetingRecord greetingRecord)
+    {
+        logger.log(INFO, "Creating greeting %s".formatted(greetingRecord.message()));
+        try
+        {
+            var persistedGreeting = greetingService.persistGreeting(greetingRecord);
+            return new GreetingRecord(persistedGreeting.getMessage(), LocalDate.now());
+        }
+        catch(IllegalArgumentException argumentException)
+        {
+            logger.log(INFO, "Error creating Greeting %s".formatted(greetingRecord
+                       .message()));
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+    }
 
-    // Inject the Greetings Service EJB
-
-    // Add a method that responds to a HTTP GET and returns a greeting record
-    // received from the Greeting Service
-
-    // Add a method that responds to HTTP POST, consumes APPLICATION/JSON,
-    // Maps it to a Greeting Record and saves it using the GreetingRecord
+    /**
+     * Returns a Persisted GreetingRecord. If note persisted record is find returns the
+     * default record
+     *
+     * @return a greeting record
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public GreetingRecord getGreetingRecord()
+    {
+        return greetingService.findFirstGreetingRecord();
+    }
 
 }
